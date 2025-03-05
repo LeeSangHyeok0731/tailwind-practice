30863e53dda32af1e8386e6197dc501cd71ec492
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-03-05 23:45:34 +0900

    flex 연습

---
+++ src/app/flex/page.tsx
@@ -0,0 +1,20 @@
+export default function Flex() {
+  return (
+    <>
+      <div className="flex justify-center items-center ">
+        <div className="bg-red-700 w-20 h-11 text-white flex justify-center items-center ">
+          빨간색
+        </div>
+        <div className="bg-amber-400 w-20 h-11 text-white flex justify-center items-center ">
+          노란색
+        </div>
+        <div className="bg-blue-600 w-20 h-11 text-white flex justify-center items-center ">
+          파란색
+        </div>
+        <div className="bg-green-700 w-20 h-11 text-white flex justify-center items-center ">
+          초록색
+        </div>
+      </div>
+    </>
+  );
+}
