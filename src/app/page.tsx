f249507097b1d45262cb7c279466fd57cc582e6b
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-03-05 23:49:12 +0900

    정렬 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import Flex from "./flex/page";
 
 export default function Home() {
   return (
-    <>
+    <div className="flex items-center flex-col">
       <Flex />
-    </>
+      <div>adfoasidjfl</div>
+    </div>
   );
 }
