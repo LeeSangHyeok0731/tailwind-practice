76e6a93c46dc9a237202b7d864549a2c7bcc0edf
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-03-10 22:37:10 +0900

    grid 적용

--- src/app/grid/page.tsx
+++ src/app/grid/page.tsx
@@ -1,20 +1,18 @@
 export default function Grid() {
   return (
-    <>
-      <div className="grid-cols-5">
-        <div className="bg-red-700 w-20 h-11 text-white flex justify-center items-center ">
-          빨간색
-        </div>
-        <div className="bg-amber-400 w-20 h-11 text-white flex justify-center items-center ">
-          노란색
-        </div>
-        <div className="bg-blue-600 w-20 h-11 text-white flex justify-center items-center ">
-          파란색
-        </div>
-        <div className="bg-green-700 w-20 h-11 text-white flex justify-center items-center ">
-          초록색
-        </div>
+    <div className="grid-cols-2 grid-rows-3">
+      <div className="bg-red-700 w-20 h-11 text-white flex justify-center items-center ">
+        빨간색
       </div>
-    </>
+      <div className="bg-amber-400 w-20 h-11 text-white flex justify-center items-center ">
+        노란색
+      </div>
+      <div className="bg-blue-600 w-20 h-11 text-white flex justify-center items-center ">
+        파란색
+      </div>
+      <div className="bg-green-700 w-20 h-11 text-white flex justify-center items-center ">
+        초록색
+      </div>
+    </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Flex from "./flex/page";
+import Grid from "./grid/page";
 
 export default function Home() {
   return (
     <div className="flex items-center flex-col">
       <Flex />
-      <div>adfoasidjfl</div>
+      <Grid />
     </div>
   );
 }
